47e4993147248e3968914e3d5fb519ac497bf4dc
Putri Harum Sari <92102385+putriharumsari@users.noreply.github.com>
2023-01-03 23:24:01 +0100

    okta: main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import OktaVue from '@okta/okta-vue'
+import { oktaAuth } from '@/okta'
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import 'bootstrap/dist/js/bootstrap.min'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(OktaVue, {
+    oktaAuth,
+    onAuthRequired: () => {
+      router.push('/login')
+    },
+    onAuthResume: () => {
+      router.push('/login')
+    }
+  })
+  .mount('#app')
